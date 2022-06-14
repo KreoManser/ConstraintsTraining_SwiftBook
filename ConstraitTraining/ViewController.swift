7629719c3ca5cabbd9f0f6b7c5799de6633d89c9
Sergey Babich <88515146+KreoManser@users.noreply.github.com>
2022-06-14 17:35:55 +0300

    Setup project and complete task

---
+++ ConstraitTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConstraitTraining
+//
+//  Created by Сергей Бабич on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
